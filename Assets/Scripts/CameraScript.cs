5694cd032f699341fc522a6c23873b4c4f073b6a
Spambot <spambot@outlook.dk>
2018-11-22 13:06:05 +0100

    Kill me

---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public List<GameObject> targets;
+    public Camera cam;
+    public Vector3 camTarget;
+    public Vector3 startPos;
+    // Start is called before the first frame update
+    void Start()
+    {
+        cam = Camera.main;
+     //   startPos = cam.transform.position;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (targets.Count > 1) {
+            camTarget = targets[0].transform.position + (targets[1].transform.position - targets[0].transform.position)/2;
+                }
+
+        cam.transform.position = camTarget + startPos;
+    }
+}
---
+++ Assets/Scripts/RagdollController.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RagdollController : MonoBehaviour
+{
+    public Rigidbody leftArm;
+    public Rigidbody rightArm;
+    public CharacterJoint leftElbow;
+    public CharacterJoint rightElbow;
+    SoftJointLimit leftLimit;
+    SoftJointLimit leftLimit2;
+    SoftJointLimit rightLimit;
+    public float velocity = 500;
+    public float limitChange = 1;
+    public float x;
+    public float y;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftLimit = leftElbow.lowTwistLimit;
+        leftLimit2 = leftElbow.highTwistLimit;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+        
+                if (Input.GetMouseButton(0))
+                {
+                    leftArm.AddForceAtPosition(transform.forward * velocity, leftArm.transform.position);
+                    leftLimit.limit = Mathf.Clamp(leftLimit.limit - limitChange, -170,170);
+                    print(leftLimit.limit);
+                    leftLimit2.limit = leftLimit2.limit - limitChange;
+                }
+              //  else { leftLimit.limit = leftLimit.limit + limitChange; leftLimit2.limit = leftLimit2.limit + limitChange; }
+
+                if (Input.GetMouseButton(1)) { rightArm.AddForceAtPosition(transform.forward * velocity, rightArm.transform.position); }
+               
+
+
+      //  if (Input.GetMouseButton(0)) leftArm.AddTorque(transform.forward * velocity, ForceMode.Acceleration);
+      //  if (Input.GetMouseButton(1)) rightArm.AddTorque(transform.forward * velocity, ForceMode.Acceleration);
+        /*
+          if (Input.GetMouseButton(0)) leftArm.AddTorque(-transform.up * velocity, ForceMode.Acceleration);
+        if (Input.GetMouseButton(1)) rightArm.AddTorque(transform.up * velocity, ForceMode.Acceleration);*/
+    }
+}
